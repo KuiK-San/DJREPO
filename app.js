4a0622b9274a3c3eacf68a8dc4fbaf743e5fd715
Guilherme Casagrande <thegui4000@gmail.com>
2024-03-28 20:44:09 -0300

    feat: enviando com imagens

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const wppconnect = require("@wppconnect-team/wppconnect");
 const csv = require("csvtojson");
+const { exit } = require("process");
 const { rimraf } = require("rimraf");
 require("dotenv").config();
 
@@ -13,10 +14,34 @@ wppconnect
 async function start(client) {
   await setTimeout(async () => {
     let path = process.env.CSV_FILE;
-    csv()
+    await mandarMensagens(path, client)
+  }, 10000);
+  exit
+}
+
+function formatarNumero(numero) {
+  const regex = /\+?(\d{2})?\s*(\d{2})?\s*(\d{4,5})\s*-*\s*(\d{4})/;
+
+  const match = regex.exec(numero);
+  if (match) {
+    let codigoPais = match[1] ? match[1] : "55";
+    let codigoArea = match[2] ? match[2] : "41";
+    if (match[3].length > 4) {
+      match[3] = match[3].slice(1)
+    }
+    let numeroFormatado = `${codigoPais}${codigoArea}${match[3]}${match[4]}@c.us`;
+    return numeroFormatado;
+  } else {
+    return null;
+  }
+}
+
+const mandarMensagens = async (path, client) => {
+  csv()
       .fromFile(path)
       .then((result) => {
         result.forEach(async (contato)  => {
+          
           let nome = contato.Name;
           const fraseCompleta = process.env.MENSAGEM;
           const partesDaFrase = fraseCompleta.split("${nome}");
@@ -29,23 +54,20 @@ async function start(client) {
               mensagem += nome.split(" ")[0] + partesDaFrase[frase];
             }
           }
+
+          //console.log(`você vai enviar mensagem para o numero ${formatarNumero(contato['Phone 1 - Value'])} com a seguinte mensagem ${mensagem}`)
+
+          if (typeof process.env.IMAGEM !== "undefined"){
+            console.log(`enviando para o contato ${nome} com imagem`)
+            await client.sendImage(formatarNumero(contato['Phone 1 - Value']), process.env.IMAGEM, 'imagemflyer', mensagem);
+            
+          }else {
+            console.log(`enviando para o contato ${nome} sem imagem`)
+            await client.sendText(formatarNumero(contato['Phone 1 - Value']), mensagem);
+
+          }
           
-          await client.sendText('554197715962@c.us', mensagem);
         });
       });
-  }, 10000);
 }
 
-function formatarNumero(numero) {
-  const regex = /\+?(\d{2})?\s*(\d{2})?\s*(\d{4,5})\s*-*\s*(\d{4})/;
-
-  const match = regex.exec(numero);
-  if (match) {
-    let codigoPais = match[1] ? match[1] : "55";
-    let codigoArea = match[2] ? match[2] : "41";
-    let numeroFormatado = `${codigoPais}${codigoArea}${match[3]}${match[4]}@c.us`;
-    return numeroFormatado;
-  } else {
-    return null;
-  }
-}
